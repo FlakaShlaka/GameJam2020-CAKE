2b9a95a25d580fcec05107357e56a3a8e30650c7
ofeklivny <ofeklivny@gmail.com>
2020-07-18 00:45:54 +0300

    A prototype of sorts. Has both fake&not picking up of objects

---
+++ CakeJam/Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float ballspeed;
+    public Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        //float ySpeed = Input.GetAxis("Vertical");
+
+        body.AddForce(new Vector3(50f, 0, 0) * (-1 * 10f * Time.deltaTime));
+        
+        body.AddTorque(new Vector3(xSpeed, 0, 0) * (ballspeed * Time.deltaTime));
+
+    }
+}
---
+++ CakeJam/Assets/Scripts/CombineScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*void OnCollisionEnter(Collision hit)
+    {
+        Debug.Log(hit.gameObject.name);
+        if (hit.gameObject.name == "Tire" )
+        {
+            Debug.Log("tire hit");
+            //GetComponentInChildren<MeshRenderer>().enabled(false);
+            //transform.Find("Tire").gameObject.SetActive(false);
+            hit.gameObject.SetActive(false);
+        }
+        //hit.transform.parent = transform;
+    }*/
+    private void OnTriggerEnter(Collider hit)
+    {
+        //Debug.Log(hit.gameObject.name);
+        // if (hit.gameObject.name == "Tire" )
+        // {
+        //     Debug.Log("tire hit");
+        //     //GetComponentInChildren<MeshRenderer>().enabled(false);
+        //     transform.Find("OnCake_Tire").gameObject.SetActive(true);
+        //     hit.gameObject.SetActive(false);
+        // }
+        
+        if (hit.gameObject.tag == "Pickup")
+        {
+            hit.transform.parent = transform;
+        }
+
+        //hit.gameObject.SetActive(false);
+    }
+    
+        /*switch (hit.gameObject.name)
+        {
+            case "Tire": transform.Find("OnCake_Tire").gameObject.SetActive(true); break;
+            case "Shoe_1": transform.Find("OnCake_Shoe").gameObject.SetActive(true); break;
+            case "Shark": transform.Find("OnCake_Shark").gameObject.SetActive(true); break;
+            case "Hamburger": transform.Find("OnCake_Hamburger").gameObject.SetActive(true); break;
+            case "Eye": transform.Find("OnCake_Eye").gameObject.SetActive(true); break;
+            //case 1: rend.material.color = Color.cyan; break;
+        }
+        hit.gameObject.SetActive(false);*/
+    
+}
