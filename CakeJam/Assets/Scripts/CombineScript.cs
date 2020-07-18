2686ec7ef0d10fe8fb81f389c7be52937f357661
ofeklivny <ofeklivny@gmail.com>
2020-07-18 16:15:18 +0300

    Added animations for picking up objects

--- CakeJam/Assets/Scripts/CombineScript.cs
+++ CakeJam/Assets/Scripts/CombineScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CombineScript : MonoBehaviour
 {
+
+    public Animation anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,21 +17,10 @@ public class CombineScript : MonoBehaviour
     {
         
     }
-
-    /*void OnCollisionEnter(Collision hit)
-    {
-        Debug.Log(hit.gameObject.name);
-        if (hit.gameObject.name == "Tire" )
-        {
-            Debug.Log("tire hit");
-            //GetComponentInChildren<MeshRenderer>().enabled(false);
-            //transform.Find("Tire").gameObject.SetActive(false);
-            hit.gameObject.SetActive(false);
-        }
-        //hit.transform.parent = transform;
-    }*/
+    
     private void OnTriggerEnter(Collider hit)
     {
+        //Not by switch case
         //Debug.Log(hit.gameObject.name);
         // if (hit.gameObject.name == "Tire" )
         // {
@@ -39,23 +30,30 @@ public class CombineScript : MonoBehaviour
         //     hit.gameObject.SetActive(false);
         // }
         
-        if (hit.gameObject.tag == "Pickup")
-        {
-            hit.transform.parent = transform;
-        }
+        //Logic for picking up items by collider
+        // if (hit.gameObject.tag == "Pickup")
+        // {
+        //     hit.transform.parent = transform;
+        // }
 
-        //hit.gameObject.SetActive(false);
-    }
-    
-        /*switch (hit.gameObject.name)
+        
+        
+        //Logic for faking picking up
+        switch (hit.gameObject.name)
         {
-            case "Tire": transform.Find("OnCake_Tire").gameObject.SetActive(true); break;
+            case "Tire": transform.Find("OnCake_Tire").gameObject.SetActive(true);
+                //anim = hit.GetComponent<Animation>();
+                //anim.Play();
+                //Debug.Log(anim.name);
+                break;
             case "Shoe_1": transform.Find("OnCake_Shoe").gameObject.SetActive(true); break;
             case "Shark": transform.Find("OnCake_Shark").gameObject.SetActive(true); break;
             case "Hamburger": transform.Find("OnCake_Hamburger").gameObject.SetActive(true); break;
             case "Eye": transform.Find("OnCake_Eye").gameObject.SetActive(true); break;
-            //case 1: rend.material.color = Color.cyan; break;
         }
-        hit.gameObject.SetActive(false);*/
+        hit.gameObject.SetActive(false);
+    }
+    
+
     
 }
